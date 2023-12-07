1dae01f64ae23c8d887b35dc47ff725a6e478926
Ssuamje <side2850@naver.com>
2023-12-07 15:39:27 +0900

    Promise.all을 이용한 user to note mapping으로 변경, toObject() 쓰지 않고도 동작하는 것 확인

--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -19,14 +19,14 @@ export class NoteService {
     async findAllNotes() {
         // id 참조방식으로 진행한다면, 프론트엔드 단에서 데이터를 refetch하는게 낫지 않을까?
         try {
-            return this.noteModel.find().exec()
-                    .then((notes) => {
-                        console.log(notes);
-                        notes.map((note) => {
-                            const author = this.userModel.findById(note.author_id).exec();
-                            return {...note.toObject(), author}
-                        });
-                    });
+            const notes = await this.noteModel.find().exec();
+            return await Promise.all(
+                notes.map(async (note) => {
+                        const author = await this.userModel.findById(note.author_id).exec();
+                        return {...note, author}
+                })
+            );
+            
 
         } catch (error) {
             
@@ -37,10 +37,9 @@ export class NoteService {
     async createNote(noteInput: NoteInputType) {
         try {
             const { title, content, author_id } = noteInput;
-            const user = await this.userModel.findById(author_id).exec();
-            if (user === null) throw new Error("Author not found");
+            const foundUser = await this.userModel.findById(author_id).exec();
+            if (foundUser === null) throw new Error("Author not found");
             
-            console.log(user);
             const data = {
                 title,
                 content,
@@ -50,12 +49,11 @@ export class NoteService {
 
             const newNote = await this.noteModel.create(data);
             
-            console.log(newNote);
             return {
                 _id: newNote._id,
                 title,
                 content,
-                author: user,
+                author: foundUser,
             };
         } catch (error) {
             
